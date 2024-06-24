1d985c5fb4925e6b98c1c6ffda3c39e2bfc5fef7
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 17:00:56 +0200

    Add styling to nav html element

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className=" border-2 border-red-500 flexBetween max-container padding-container z-30 py-5"></nav>
+  );
 };
 
 export default Navbar;
