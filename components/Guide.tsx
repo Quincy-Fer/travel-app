fdfca6354c4072d58cc132f5ad953f34cffe9bf8
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 23:36:13 +0200

    Create layout for Guide section, add image and p

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Guide = () => {
-  return <div>Guide</div>;
+  return (
+    <section className="flexCenter flex-col">
+      <div
+        className="border-2 border-red-500 
+                    padding-container max-container w-full pb-24"
+      >
+        <Image src="camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 mb-3 text-green-50 -mt-1">We are here for you</p>
+      </div>
+    </section>
+  );
 };
 
 export default Guide;
