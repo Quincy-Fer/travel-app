81a476645cc0b72ae41133b0e708107b735b1f7f
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 18:16:37 +0200

    Create constants folder with data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,12 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container z-30 py-5">
-      <Link href="/"> <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}  />  </Link>
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden lg:flex gap-12 h-full ">
+        
+      </ul>
     </nav>
   );
 };
