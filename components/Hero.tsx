69a8a566463849835826a9163c0e1e756d756763
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 21:17:49 +0200

    Add div with stars, text and span. Style

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,8 +21,30 @@ const Hero = () => {
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of
           seeing the incorruptible beauty of nature. We can help you on an
-          adventure around the world in just one app
+          adventure around the world in just one app.
         </p>
+
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className=" flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((item, index) => (
+                <Image
+                  src="star.svg"
+                  key={index}
+                  height={24}
+                  width={24}
+                  alt="star"
+                />
+              ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198K{" "}
+            <span className="regular-16 lg:regular-20 ml-1 ">
+              Excellent Reviews
+            </span>
+          </p>
+        </div>
       </div>
     </section>
   );
