cdd7e6fbf87098b378e41e0de64a0c407fd882cc
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 17:13:53 +0200

    Add assets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <nav className=" border-2 border-red-500 flexBetween max-container padding-container z-30 py-5"></nav>
+    <nav className=" border-2 border-red-500 flexBetween max-container padding-container z-30 py-5">
+      <Link href="/"></Link>
+    </nav>
   );
 };
 
