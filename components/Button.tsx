8a39b812261f734f2a8470b5e162127160ee77a4
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 18:55:12 +0200

    Pass props to Button.tsx, define types of props

---
+++ components/Button.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button type={type}>
+      {icon && <Image src={icon} alt={title} height={24} width={24} />}
+      <label>{title}</label>
+    </button>
+  );
+};
+
+export default Button;
