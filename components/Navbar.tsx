b2857b66beb5175efef937028544e1678297d213
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 17:14:48 +0200

    Test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className=" border-2 border-red-500 flexBetween max-container padding-container z-30 py-5">
-      <Link href="/"></Link>
+      <Link href="/">  </Link>
     </nav>
   );
 };
