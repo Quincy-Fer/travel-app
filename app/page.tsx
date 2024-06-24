fbf75df8ec6aecdc3a7771a23d4c69ccb02646aa
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-24 16:28:42 +0200

    Add metadata, remove Inter font

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <h1 className="font-bold text-teal-500">Hello World </h1>;
+  return (
+    <h1 className="font-bold text-teal-500 text-4xl">Hello World </h1>
+  );
 }
