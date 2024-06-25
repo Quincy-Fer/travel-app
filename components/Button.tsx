7b6f7239ccf91dae3a93f117b4efe84ea3d01a9d
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-25 12:14:23 +0200

    Add header, p and button to CTA, add new type to button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,11 +5,12 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => { 
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => { 
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full ${variant}`}>
+    <button type={type} className={`flexCenter gap-3 rounded-full ${variant} ${full && 'w-full'}`}>
       {icon && <Image src={icon} alt={title} height={24} width={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
