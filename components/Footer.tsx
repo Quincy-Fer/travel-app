ffc76b7441fba55cb25bc5b4fe8da9b878296207
Quincy Fer <quincy.e.fer@gmail.com>
2024-06-25 13:41:23 +0200

    Finish Cta sections, Start Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,38 @@
+import { FOOTER_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex flex-col gap-14 w-full">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+          </Link>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map(columns => (
+              <FooterColumn title={columns.title} > </FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
+
+type FooterColumnProps = {
+  title:string;
+  children: React.ReactNode;
+}
+
+const FooterColumn = ({title, children}:FooterColumnProps) => {
+  return (
+    <div>
+      <h4>{title}</h4>
+      {children}
+    </div>
+  )
+}
 
 export default Footer;
